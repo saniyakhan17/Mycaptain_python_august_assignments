ec5d7ff2ce317e0f1edd23013449c8180e5e7fe1
Saniya Khan <80807406+saniyakhan17@users.noreply.github.com>
2021-08-17 20:41:03 +0530

    Add files via upload

---
+++ mostoccuredstring.py
@@ -0,0 +1,10 @@
+st= input('Please enter a string')
+def most_occ(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+print (most_occ(st))
