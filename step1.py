0838cee1bf215a797e016a619c3846a1e0d4a1c1
Saniya Khan <saniyakhan1709@gmail.com>
2021-08-26 08:30:19 +0530

    when the user enters the wrong information
    added a proper display of info for the check

--- step1.py
+++ step1.py
@@ -11,13 +11,21 @@ if __name__ == '__main__':
     condition= True
     while(condition==True):
         s_info=input("Enter the name and class of the student in the format:(name, age, contact_number, email id): ")
-        print("Entered informataion: "+ s_info)
+        
         s_info_split=s_info.split(' ')
-        write_into_csv(s_info_split)
-        print("Entered splitted information: " + str(s_info_split))
-        check=input("Enter y if you want to enter student info again and n if you dont:" )
-        if(check=="y"):
-            condition=True
-        elif(check=="n"):
-            condition=False
+        print("\nEntered information is: \n Name:{}\n Age:{}\n Phone numbe:{}\n Email ID:{}".format(s_info_split[0],s_info_split[1],s_info_split[2],s_info_split[3]))
+        choice_check=input("is the entered info correct>(yes/no)")
+        
+        if choice_check=="yes":
+            write_into_csv(s_info_split)
+            check=input("Enter y if you want to enter student info again and n if you dont:" )
+            if(check=="y"):
+                condition=True
+            elif(check=="n"):
+                condition=False
+        elif choice_check=="no":
+            print("\n please renter the information")
+        
+        
+
     
