8d4ee8dfd8dd6d591b5b8f8cab964d1682e867c2
Saniya Khan <saniyakhan1709@gmail.com>
2021-08-27 14:03:05 +0530

    Add files via upload

---
+++ scraper1.py
@@ -0,0 +1,13 @@
+#web scrapping using the libraries request and beautifulsoup
+import requests
+from bs4 import BeautifulSoup
+gsoc_url="https://summerofcode.withgoogle.com/organizations/?sp-search=c%2B%2B"
+req=requests.get(gsoc_url)
+content=req.content
+
+soup = BeautifulSoup(content,"html.parser")
+all_organization=soup.find_all("md-card",{"class": "organization-card"})
+for org in all_organization:
+    org_name=org.find("div",{"class": "org__logo"}).text
+    org_desc=org.find("div",{"class": "organiztion-card__name"}).text
+    print(org_name, org_desc)
